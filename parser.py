eaad3c61b41a4898b97bba72ef21214255d16188
Marek Puzyniak <marek.puzyniak@tieto.com>
2014-03-10 22:09:01 +0100

    parser: continue
    Signed-off-by: Marek Puzyniak <marek.puzyniak@tieto.com>

--- parser.py
+++ parser.py
@@ -172,14 +172,71 @@ def count_factor(pfiles, sta1_mac, sta2_mac, term_mac):
 	
 	print "current time " + str(curr_time)
 	print "rtp3 data " + str(rtp3_data)
-	rate = float(rtp3_data) / float(curr_time - start_time)
-	rate = rate / 1024
+	rtp2_rate = float(rtp3_data) / float(curr_time - start_time)
+	rtp3_rate = rtp3_rate / 1024
 	print " "
-	print "RTP3 rate " + str(rate) + " KBps"
+	print "RTP3 rate " + str(rtp3_rate) + " KBps"
+
+	#RTP1
+	end_time = curr_time
+	qosdata_flr = "wlan.fc.type_subtype == 0x28"
+	src_flr = "wlan.sa == " + term_mac
+	dst_flr = "wlan.da == " + sta1_mac
+	retry_flr = "wlan.fc.retry == 1"
+	flr = qosdata_flr + " and " + src_flr + " and " + dst_flr + " and !" + retry_flr
+
+	tshark_cmd = []
+	tshark_cmd.append("tshark")
+	tshark_cmd.append("-2")
+	tshark_cmd.append("-r%s" % pfiles.get_input_path())
+	tshark_cmd.append("-R%s" % flr)
+	tshark_cmd.append("-Tfields")
+	tshark_cmd.append("-Eheader=y")
+	tshark_cmd.append("-Eseparator=" + sep)
+	tshark_cmd.append("-eframe.number")
+	tshark_cmd.append("-edata.len")
+	tshark_cmd.append("-eframe.time_relative")
+	#tshark_cmd.append("-w%s" % pfiles.get_output_path())
+
+	print tshark_cmd
+	# filter raw pcap file
+	result = []
+	process = Popen(tshark_cmd, stdout=PIPE, stderr=PIPE)
+        (result, error_msg) = process.communicate()
+	if error_msg:
+		print "ERROR:"
+		print error_msg
+
+	lines = result.splitlines()
+	i = int(1)
+	curr_time = float(0)
+
+	while curr_time < start_time:
+		(fr_nr, dlen, time) = lines[i].split(' ', 2)
+		i += 1
+		curr_time = float(time)
+
+	rtp1_data = int(0)
+
+	while curr_time < end_time:
+		(fr_nr, dlen, time) = lines[i].split(' ', 2)
+		if not dlen:
+			print "No data "
+			break
+		rtp1_data += int(dlen)
+		curr_time = float(time)
+		i += 1
 
 	
+	print "rtp1 data " + str(rtp1_data)
+	rtp1_rate = float(rtp1_data) / float(curr_time - start_time)
+	rtp1_rate = rtp1_rate / 1024
+	print " "
+	print "RTP1 rate " + str(rtp1_rate) + " KBps"
 
-	return result
+	rate = float(rtp1_rate / rtp3_rate)
+
+	return rate
 
 #######################
 #scr_mac = "00:24:14:54:92:00"
@@ -208,9 +265,9 @@ if __name__ == "__main__":
 	for ap in ap_list:
 		ap.show_info()
 
-	nr_data = count_factor(pfiles, sta1_mac, sta2_mac, term_mac)
+	rate = count_factor(pfiles, sta1_mac, sta2_mac, term_mac)
 	print ""
-	#print nr_data
+	print "Rate factor [" + rate + "]"
 
 
 
